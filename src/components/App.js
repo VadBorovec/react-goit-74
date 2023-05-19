00919afe7cd84025c44d115101fd0504c5eb3855
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-05-19 12:56:36 +0300

    update components

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import { RecipeList } from './RecipeList';
+import recipes from '../recipes.json';
+
+export const App = () => {
+  return (
+    <>
+      <RecipeList items={recipes} />
+    </>
+  );
+};
+
+// !====================
+// * react 22
+
+import PaintingList from './PaintingList';
+import Section from './Section';
+import paintings from './paintings.json';
+
+export default function App() {
+  return (
+    <div>
+      <Section title="Топ недели">
+        <PaintingList items={paintings} />
+      </Section>
+
+      <Section title="Лучшее"></Section>
+    </div>
+  );
+}
